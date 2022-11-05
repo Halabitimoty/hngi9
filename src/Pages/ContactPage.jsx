9b4bf965ff09ac99b7a33cc6f70cbe9b391fd4ac
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-11-05 02:38:10 +0100

    Clean ups, Pages added and Form Created

---
+++ src/Pages/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from '../component/Form/Form'
+
+function ContactPage() {
+  return (
+	<>
+		<Form />
+	</>
+  )
+}
+
+export default ContactPage
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Profile from './../component/Profile/Profile';
+import Links from './../component/Links/Links';
+
+
+function HomePage() {
+  return (
+	<>
+		<Profile/>
+		<Links/>
+	</>
+  )
+}
+
+export default HomePage
