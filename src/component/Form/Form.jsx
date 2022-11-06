d42737bf59d195ac38e42f95e9a49b6c67fa6a4f
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-11-06 12:35:27 +0100

    FormData State Management Updated

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -1,48 +1,97 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import "./Form.css";
 function Form() {
+  const [formdata, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    message: "",
+    check: false,
+  });
 
- const [handle, setHandle] = useState("");
- 
- function handleChange()
- {
+  //   console.log(formdata);
 
- }
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
 
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
-	<div className='Formpage'>
-		<h3>Contact Me</h3>
-		<p>Hi there, contact me to ask me about anything you have in mind.</p>
-		<form>
-			<div className="user_name">
-				<div>
-					<label htmlFor="first_name">First name</label>
-					<input type="text" name="first_name" id="" onChange={handleChange} placeholder="Enter your first name" />
-				</div>
-				<div>
-					<label htmlFor="last_name">Last name</label>
-					<input type="text" name="last_name" id="" onChange={handleChange} placeholder="Enter your last name" />
-				</div>
-			
-			</div>
-			<div className='user_details'>
-				<label htmlFor="email">Email</label>
-				<input type="email" name="email" id="" onChange={handleChange} placeholder="yourname@email.com"/>
-				
-				<label htmlFor="message">Message</label>
-				<input type="text" name="message" id="" onChange={handleChange} placeholder="Send me a message and I'll reply you as soon as possible..."/>
-				
-				<div className='user_check'>
-					<input type="checkbox" name="check" id="" onChange={handleChange} placeholder=""/>
-					<label htmlFor="check">You agree to providing your data to name who may contact you.</label>
-				</div>
-			</div>
+    <div className="Formpage">
+      <h3>Contact Me</h3>
+      <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      <form onSubmit={handleSubmit}>
+        <div className="user_name">
+          <div>
+            <label htmlFor="first_name">First name</label>
+            <input
+              type="text"
+              name="first_name"
+              id=""
+              onChange={handleChange}
+              placeholder="Enter your first name"
+              value={formdata.first_name}
+            />
+          </div>
+          <div>
+            <label htmlFor="last_name">Last name</label>
+            <input
+              type="text"
+              name="last_name"
+              id=""
+              onChange={handleChange}
+              placeholder="Enter your last name"
+              value={formdata.last_name}
+            />
+          </div>
+        </div>
+        <div className="user_details">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id=""
+            onChange={handleChange}
+            placeholder="yourname@email.com"
+            value={formdata.email}
+          />
 
-			<input type="submit" value="Send Message" className='submit' />
-		</form>
-	</div>
-  )
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id=""
+            onChange={handleChange}
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+            value={formdata.message}
+          />
+
+          <div className="user_check">
+            <input
+              type="checkbox"
+              name="check"
+              id="is_checked"
+              onChange={handleChange}
+              placeholder=""
+              checked={formdata.check}
+            />
+            <label htmlFor="is_checked">
+              You agree to providing your data to name who may contact you.
+            </label>
+          </div>
+        </div>
+
+        <input type="submit" value="Send Message" className="submit" />
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
